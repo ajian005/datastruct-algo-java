4c1dc1f8e8e1598700a7e8b4c2d28644e8633d5c
ajian005 <79331356@qq.com>
2024-03-02 20:33:48 +0800

    junit

--- src/main/java/com/HelloWorld.java
+++ src/main/java/com/HelloWorld.java
@@ -1,7 +1,14 @@
 package com;
 
+import org.junit.Test;
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+    }
+
+    @Test
+    public void testA() {
+        System.out.println("sadfasdfasd");
     }
 }
