ffb44ee953e4d532bad6252a6dbc27fbc7fef846
ajian005 <79331356@qq.com>
2024-03-29 14:19:42 +0800

    新增节点

--- src/main/java/com/data/struct/basic/tree/BinaryTree.java
+++ src/main/java/com/data/struct/basic/tree/BinaryTree.java
@@ -1,5 +1,8 @@
 package com.data.struct.basic.tree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * 定义一个二叉树
  */
@@ -65,6 +68,112 @@ public class BinaryTree {
         // the recur on right subtree
         preOrder(node.right);
     }
+
+    /**
+     * Given a binary tree, print its nodes in levelorder
+     */
+    public void levelOrder1(Node node) {
+        int hight = maxDepth(node);
+        for (int i = 0; i <= hight; i++) {
+            printCurrentLevel(node, i);
+        }
+
+    }
+
+    /**
+     * Print nodes at the current level 递归版
+     * @param root
+     * @param level
+     */
+    public void printCurrentLevel(Node root, int level) {
+        if (root == null) {
+            return;
+        }
+        if (level == 1) {
+            System.out.print(root.key + " ");
+        } else if(level > 1) {
+            printCurrentLevel(root.left, level - 1);
+            printCurrentLevel(root.right, level - 1);
+        }
+    }
+
+    /**
+     * Given a binary tree.
+     * Print its nodes in level order using array for implementing queue
+     * @param root
+     */
+    public void printLevelOrderByQueue(Node root) {
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            // poll() removes the present head.
+            Node tempNode = queue.poll();
+            System.out.print(tempNode.key + " ");
+
+            // Enqueue left child
+            if (tempNode.left != null) {
+                queue.add(tempNode.left);
+            }
+
+            // Enqueue right child
+            if (tempNode.right != null) {
+                queue.add(tempNode.right);
+            }
+        }
+    }
+
+    /**
+     * Compute the "maxDepth" of a tree
+     * -- the number of nodes along the longest path from the root node down to the farthest leaf node.
+     */
+    int maxDepth(Node node) {
+        if (node == null) {
+            return 0;
+        } else {
+            /* compute the depth of each subtree */
+            int lDepth = maxDepth(node.left);
+            int rDepth = maxDepth(node.right);
+            /* use the large one */
+            if (lDepth > rDepth) {
+                return lDepth + 1;
+            } else {
+                return rDepth + 1;
+            }
+        }
+    }
+
+    /**
+     * insertion-in-a-binary-tree-in-level-order https://www.geeksforgeeks.org/insertion-in-a-binary-tree-in-level-order/
+     * function to insert element in binary tree
+     * @return
+     */
+    public void insert(Node temp, int key) {
+        if (temp == null) {
+            root = new Node(key);
+            return;
+        }
+        Queue<Node> q = new LinkedList<Node>();
+        q.add(temp);
+
+        // Do level order traversal until we find an empty place.
+        while (!q.isEmpty()) {
+            temp = q.peek();
+            q.remove();
+            if (temp.left == null) {
+                temp.left = new Node(key);
+                break;
+            } else {
+                q.add(temp.left);
+            }
+            if (temp.right == null) {
+                temp.right = new Node(key);
+            } else {
+                q.add(temp.right);
+
+            }
+        }
+    }
+
 
     public BinaryTree createBinaryTree() {
         BinaryTree tree = new BinaryTree();
--- src/main/java/com/data/struct/basic/tree/BinaryTreeMain.java
+++ src/main/java/com/data/struct/basic/tree/BinaryTreeMain.java
@@ -1,9 +1,20 @@
 package com.data.struct.basic.tree;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class BinaryTreeMain {
     public static void main(String[] args) {
         BinaryTree binaryTreeDemo = new BinaryTree();
         BinaryTree binaryTree = binaryTreeDemo.createBinaryTree();
         binaryTreeDemo.preOrder(binaryTree.root);
+    }
+
+    @Test
+    public void BinaryTreeMaxDepth() {
+        BinaryTree binaryTreeDemo = new BinaryTree();
+        BinaryTree binaryTree = binaryTreeDemo.createBinaryTree();
+        int maxDepth = binaryTree.maxDepth(binaryTree.root);
+        Assert.assertEquals("二叉树最大高度", 3, maxDepth);
     }
 }
